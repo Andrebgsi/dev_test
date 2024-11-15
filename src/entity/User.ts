6666b80f0d51d5a8b27dafdb612dd5588ce6fb3d
Bruce <andrebgsi@gmail.com>
2024-11-15 15:08:47 -0300

    feat: Created User entity

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
-//TODO Crie a entidade de User
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number | undefined;
+
+  @Column({ type: "varchar", length: 100 })
+  firstName: string | undefined;
+
+  @Column({ type: "varchar", length: 100 })
+  lastName: string | undefined;
+
+  @Column({ type: "varchar", length: 100, unique: true })
+  email: string | undefined;
+
+  @OneToMany(() => Post, (post: { user: any; }) => post.user)
+  posts: Post[] | undefined;
+}
+
