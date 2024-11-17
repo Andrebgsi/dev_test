09bc5b31f98c8a7af7794bba9964b7513a2577f0
Bruce <andrebgsi@gmail.com>
2024-11-17 15:30:36 -0300

    feat: EXTRA - router updatePost

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createPost, getPosts, getPostById } from "../controllers/postController";
+import { createPost, getPosts, getPostById, updatePost } from "../controllers/postController";
 
 const router = Router();
 
 router.get("/", getPosts); // Listar todos os posts
 router.get("/:id", getPostById); // Obter um post pelo ID
 router.post("/", createPost); // Criar um novo post
+router.put("/:id", updatePost); // Atualizar um post pelo ID
 
 export default router;
