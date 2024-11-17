7872975c45ef0d35dbba84c374c99b610749cbeb
Bruce <andrebgsi@gmail.com>
2024-11-17 09:12:05 -0300

    refactor: Enity User

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -4,18 +4,17 @@ import { Post } from "./Post";
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number | undefined;
+  id!: number;
 
-  @Column({ type: "varchar", length: 100 })
-  firstName: string | undefined;
+  @Column({ type: "varchar", length: 100, nullable: false })
+  firstName!: string;
 
-  @Column({ type: "varchar", length: 100 })
-  lastName: string | undefined;
+  @Column({ type: "varchar", length: 100, nullable: false })
+  lastName!: string;
 
-  @Column({ type: "varchar", length: 100, unique: true })
-  email: string | undefined;
+  @Column({ type: "varchar", length: 100, nullable: false, unique: true })
+  email!: string;
 
-  @OneToMany(() => Post, (post: { user: any; }) => post.user)
-  posts: Post[] | undefined;
+  @OneToMany(() => Post, (post) => post.user)
+  posts!: Post[];
 }
-
