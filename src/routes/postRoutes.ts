9dd45ac984fe4a588e1b028d985b4d9e439b153d
Bruce <andrebgsi@gmail.com>
2024-11-17 15:10:08 -0300

    feat: EXTRA - router gePosts

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { createPost } from "../controllers/postController";
+import { createPost, getPosts } from "../controllers/postController";
 
 const router = Router();
 
+router.get("/", getPosts); // Listar todos os posts
 router.post("/", createPost); // Criar um novo post
 
 export default router;
