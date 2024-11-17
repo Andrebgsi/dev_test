6a4e449db3a8dcb8cc908ed2838ebcd9753fe475
Bruce <andrebgsi@gmail.com>
2024-11-17 15:01:45 -0300

    feat: router getUsers

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { createUser } from "../controllers/userController";
+import { createUser, getUsers } from "../controllers/userController";
 
 const router = Router();
 
-
+router.get("/", getUsers); // Listar todos os usuários
 router.post("/", createUser); // Criar um novo usuário
 
 
