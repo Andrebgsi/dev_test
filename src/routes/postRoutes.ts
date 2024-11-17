56774f3d97478fba9491b624f53b2af703b2e561
Bruce <andrebgsi@gmail.com>
2024-11-17 15:20:43 -0300

    feat: EXTRA - router getPostById

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/postController";
+import { createPost, getPosts, getPostById } from "../controllers/postController";
 
 const router = Router();
 
 router.get("/", getPosts); // Listar todos os posts
+router.get("/:id", getPostById); // Obter um post pelo ID
 router.post("/", createPost); // Criar um novo post
 
 export default router;
