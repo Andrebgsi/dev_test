a30687fcc2578889c0723d09ed25c907128f2560
Bruce <andrebgsi@gmail.com>
2024-11-17 14:58:58 -0300

    feat: Remove dist

--- dist/routes/postRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const postController_1 = require("../controllers/postController");
-const router = (0, express_1.Router)();
-router.post("/", postController_1.createPost); // Criar um novo post
-exports.default = router;
