558673c9bef5d9a3b57ccb9828d24ecca4b69fdd
Bruce <andrebgsi@gmail.com>
2024-11-17 15:11:35 -0300

    feat: EXTRA - router getUserById

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/userController";
+import { createUser, getUsers, getUserById } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", getUsers); // Listar todos os usuários
+router.get("/:id", getUserById); // Obter um usuário pelo ID
 router.post("/", createUser); // Criar um novo usuário
 
 
