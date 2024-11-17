935e9f2f212d1302ce784d8f4aaec80e555a67d3
Bruce <andrebgsi@gmail.com>
2024-11-17 15:28:57 -0300

    feat: EXTRA - router updateUser

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createUser, getUsers, getUserById } from "../controllers/userController";
+import { createUser, getUsers, getUserById, updateUser } from "../controllers/userController";
 
 const router = Router();
 
 router.get("/", getUsers); // Listar todos os usuários
 router.get("/:id", getUserById); // Obter um usuário pelo ID
 router.post("/", createUser); // Criar um novo usuário
+router.put("/:id", updateUser); // Atualizar um usuário pelo ID
 
 
 export default router;
